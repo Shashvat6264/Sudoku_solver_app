1d113dba6dad13a2b307aa73e03f2f0fc33ecc32
Shashvat6264 <shashvat6264@gmail.com>
2020-06-29 04:12:27 +0530

    Takes and saves pics

--- app/src/main/java/com/example/sudoku_solver/MainActivity.java
+++ app/src/main/java/com/example/sudoku_solver/MainActivity.java
@@ -2,8 +2,24 @@ package com.example.sudoku_solver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,15 +28,44 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("native-lib");
     }
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        Button click = (Button) findViewById(R.id.click);
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent takepictureintent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takepictureintent.resolveActivity(getPackageManager()) != null){
+                    startActivityForResult(takepictureintent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ImageView picture = (ImageView) findViewById(R.id.showpic);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            picture.setImageBitmap(imageBitmap);
+            String savedImageURL = MediaStore.Images.Media.insertImage(
+                    getContentResolver(),
+                    imageBitmap,
+                    "Sudoku",
+                    "Image of Sudoku"
+            );
+            Uri savedImageURI = Uri.parse(savedImageURL);
+            picture.setImageURI(savedImageURI);
+//            createDirectoryAndSave(imageBitmap, new String("Sudoku"));
+        }
+    }
+
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
